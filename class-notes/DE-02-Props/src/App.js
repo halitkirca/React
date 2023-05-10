40cb3ebb765591f34b2e596d3a752351100f94b0
halit <halit.krca@gmail.com>
2023-05-10 11:12:43 +0200

    props

---
+++ class-notes/DE-02-Props/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Person from "./Person";
+const data = veriler
+
+function App() {
+  return (
+    <div>
+      <Person dataa={data}/>
+    </div>
+  );
+}
+
+export default App;
