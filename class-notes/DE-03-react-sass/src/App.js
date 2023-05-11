1eb3759410fa3ef2a12efae24671aa8c9cd76bc2
halit <halit.krca@gmail.com>
2023-05-11 13:57:29 +0200

    sass

--- class-notes/DE-03-react-sass/src/App.js
+++ class-notes/DE-03-react-sass/src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React from "react"
+// import './scss/_button.scss'
+// import './scss/_reset.scss'
+// import './scss/_variable.scss'
 
+import "./App.scss"
+import Header from "./components/header/Header";
+import Card from "./components/card/Card";
+import dataa from "./data"
 function App() {
   return (
-    <div>
-      app
+    <div >
+    <Header/>
+    <Card datA={dataa}/>
     </div>
   );
 }
