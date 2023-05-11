e13ab8403007f67d5f449e532a84ed8ea5cb6a41
halit <halit.krca@gmail.com>
2023-05-11 13:53:02 +0200

    cards

--- class-notes/DE-03-react-bootstrap/src/App.js
+++ class-notes/DE-03-react-bootstrap/src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import MyNavbar from "./components/MyNavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./components/Footer";
+import dataa from "./data";
+import CourseCard from "./components/CourseCard";
 
 function App() {
   return (
     <div>
       <MyNavbar />
+
+      <CourseCard data={dataa} />
 
       <Footer />
     </div>
