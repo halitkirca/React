69ea733d5a5cc66394c1850891a18c56988e7968
halit <halit.krca@gmail.com>
2023-05-28 19:51:38 +0200

    react router dom

--- class-notes/DE-12-11-react-router/src/App.js
+++ class-notes/DE-12-11-react-router/src/App.js
@@ -9,6 +9,8 @@ import Teacher from './pages/Teacher';
 import CourseCard from './pages/CourseCard';
 import ContactForm from './pages/ContactForm';
 import NotFound from './pages/NotFound';
+import CardDetails from './pages/CardDetails';
+import TeacherDetails from './pages/TeacherDetails';
 
 
 const App = () => {
@@ -19,10 +21,18 @@ const App = () => {
 
         <Routes>
           {/* "/" (ana yol) tüm yollara dahil edilmiştir, bu nedenle onu
-         "/" ile başlayan diğer yollardan ayırt etmek için exact anahtar kelimesine sahip olması gerekir . */}
+         "/" ile başlayan diğer yollardan ayırt etmek için exact anahtar kelimesine sahip olması gerekir .
+         "/courses/:name" böyle bir yolda yani  :name belirtilen kısım bir değişkendir, mesela name=HTML gibi. bunun için : kullanılır
+         
+          */}
           <Route exact path="/" element={<Home />} />
           <Route path="/teacher" element={<Teacher />} />
+
+          <Route path="/teacher/:id" element={<TeacherDetails/>} />
           <Route path="/courses" element={<CourseCard />} />
+
+          <Route path="/courses/:namee" element={<CardDetails  />} />
+
           <Route path="/contact" element={<ContactForm />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
