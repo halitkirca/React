a26eaa141e868c21798f61e65a603661baf7d880
halit <halit.krca@gmail.com>
2023-06-05 18:21:04 +0200

    useContext

---
+++ class-notes/DE-16-Context-Reducer/context-2/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import GosterUsers from "./pages/GosterUsers";
+import { useState } from "react";
+import { useEffect } from "react";
+import { createContext } from "react";
+
+export const KullaniciContext=createContext()
+
+const App = () => {
+  const [kullanici, setKullanici] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users")
+      .then((res) => res.json())
+      .then((data) => setKullanici(data));
+  }, []);
+
+const degistirWidth=(id,yeniWidth)=>{
+ 
+setKullanici(
+  kullanici.map((a)=>a.id===id ? {...a,width:yeniWidth}:a)
+  )
+
+}
+
+
+  return (
+    <KullaniciContext.Provider value={{kullanici,degistirWidth}}>
+      <GosterUsers />
+    </KullaniciContext.Provider>
+  );
+};
+
+export default App;
---
+++ class-notes/DE-16-Context-Reducer/context-2/src/components/AnaUser.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useContext } from "react";
+import { KullaniciContext } from "../App";
+
+
+const AnaUser = ({kullanici}) => {
+//  const{kullanici,degistirWidth}=useContext(KullaniciContext)
+const {  degistirWidth } = useContext(KullaniciContext);
+console.log(kullanici);
+  return (
+    <div>
+      {kullanici.map((i) => {
+        return (
+          <div key={i.id}>
+            <h3>{i.login}</h3>
+            <img src={i.avatar_url} alt="" width={i.width} />
+
+            <div>
+              <label htmlFor={i.id}>Image width(px)</label>
+              <input
+                id={i.id}
+                type="number"
+               onChange={(e)=>degistirWidth(i.id,e.target.value)}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default AnaUser;
