b34b3d23e4ade1999cb25d3ad86f9bc283b21866
halit <halit.krca@gmail.com>
2023-06-06 20:33:39 +0200

    useref and memo

---
+++ class-notes/rc23_useref-usememo-usecallback/src/components/Users.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { memo } from 'react';
+import UserItem from './UserItem';
+
+const Users = ({kisiler}) => {
+  console.log('USERS COMPONENT RENDERED');
+
+  return (
+    <div className="users">
+      
+      {kisiler.map((user) => (
+        <UserItem key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+// buradaki memo kullanici=kullanici gönderildiğinde, inputa her veri girişinde burası render edilmemesi için
+export default memo(Users);
